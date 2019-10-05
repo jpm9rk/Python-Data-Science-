b01fa3cba7d2a0dc4a43197e8fefc0d0c16500c5
James <52896018+jpm9rk@users.noreply.github.com>
2019-10-05 15:25:59 -0400

    Add files via upload

---
+++ county_boundary/project4.py
@@ -0,0 +1,34 @@
+# James Morrissey
+# computingID: jpm9rk
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from shapely.geometry import Point, Polygon
+import geopandas as gpd
+import descartes
+from shapely.geometry import Polygon, Point
+
+
+
+
+denver = pd.read_csv('crime.csv')
+# determine if there are any missing values in a column, and if so, how many are there
+# for column in list(denver.columns):
+#     print('does', column, 'have any missing values?', denver[column].isnull().any())
+#     if denver[column].isnull().any():
+#         print('\t there are', np.sum(denver[column].isnull().values), ' missing values in this column')
+
+just_crime = denver.query('IS_CRIME == 1')
+just_traffic = denver.query('IS_TRAFFIC == 1')
+just_crime=just_crime[just_crime.GEO_LAT > 35]
+
+denver_pic = gpd.read_file('county_boundary.shp')
+fig, ax = plt.subplots(figsize=(7, 7))
+ax = plt.gca()
+geometry = [Point(xy) for xy in zip(just_crime.GEO_LON,just_crime.GEO_LAT)]
+crs={'init':'epsg:4326'}
+gdf = gpd.GeoDataFrame(just_crime, crs=crs,  geometry=geometry)
+denver_pic.plot(ax=ax,color='white',edgecolor='black')
+gdf.plot(ax=ax, color='red')
+plt.show()
---
+++ project3.py
@@ -0,0 +1,70 @@
+# James Morrissey
+# computingID: jpm9rk
+# used students.csv
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('classic')
+
+students = pd.read_csv('StudentsPerformance.csv')
+students.index.name = 'student id'
+students.columns = ['gender','race_or_ethnicity',
+                    'parental_education_level','ate_lunch',
+                    'test_prep_course','math_score','reading_score','writing _score']
+
+
+def group_converter(string):
+    """
+    Return group name as single letter.
+
+    :param string:(str) name of the group
+    :return:(str) name of the group as a single letter
+    """
+    if 'A' in string:
+        return 'A'
+    if 'B' in string:
+        return 'B'
+    if 'C' in string:
+        return 'C'
+    if 'D' in string:
+        return 'D'
+    if 'E' in string:
+        return 'E'
+    return ''
+
+
+students['race_or_ethnicity'] = students.race_or_ethnicity.apply(group_converter)
+groupA = students[students.race_or_ethnicity == 'A']
+groupB = students[students.race_or_ethnicity == 'B']
+groupC = students[students.race_or_ethnicity == 'C']
+groupD = students[students.race_or_ethnicity == 'D']
+groupE = students[students.race_or_ethnicity == 'E']
+
+# descriptive statistics for each racial group
+a_sum = groupA.describe()
+b_sum = groupB.describe()
+c_sum = groupC.describe()
+d_sum = groupD.describe()
+e_sum = groupE.describe()
+
+# math scores in particular for each racial group
+
+print('GROUP A\n')
+print(groupA.describe())
+print('GROUP B\n')
+print(groupB.describe())
+print('GROUP C\n')
+print(groupC.describe())
+print('GROUP D\n')
+print(groupD.describe())
+print('GROUP E\n')
+print(groupE.describe())
+
+x = ['Group A', 'Group B', 'Group C', 'group D', 'group E']
+y = [a_sum.math_score['mean'],b_sum.math_score['mean'],c_sum.math_score['mean'],
+     d_sum.math_score['mean'],e_sum.math_score['mean']]
+plt.bar(x, y)
+plt.xlabel('Racial/Ethnic Group')
+plt.ylabel('Mean Math Score')
+plt.title('Mean Math Scores by Racial/Ethnic Group')
+plt.show()
